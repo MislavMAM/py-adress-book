1c91332f89ffc16b3734c3971187e8a506b3c3d7
lcoric3 <luka.coric3@gmail.com>
2024-02-01 18:57:02 +0100

    json finished

---
+++ main.py
@@ -0,0 +1,18 @@
+from services.file_services import (get_last_id,
+                                    get_contact)
+
+
+if get_last_id() != -1:
+    id = get_last_id() + 1
+else:
+    id = 1
+
+print(id)
+
+id_contact = 15
+contact = get_contact(id_contact)
+if contact != None:
+    print(contact)
+    print(contact.id)
+else:
+    print(f'Ne postoji trazeni kontakt s ID: {id_contact} u bazi!')
